1397c69abf22a6ba48b3062d33cb153ff99e3cf1
thereal-atom <78076198+thereal-atom@users.noreply.github.com>
2021-06-26 21:11:46 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+let ws = new WebSocket('wss://stream.binance.com:9443/ws/etheur@trade');
+let stockPriceElement = document.getElementById('stock-price');
+let lastPrice = null;
+
+ws.onmessage = (event) => {
+    let stockObject = JSON.parse(event.data);
+    let price = parseFloat(stockObject.p).toFixed(2)
+    stockPriceElement.innerText = price;
+    stockPriceElement.style.color = !lastPrice || lastPrice === price ? 'black' : lastPrice > price ? 'red' : 'green';
+    lastPrice = price;
+}
